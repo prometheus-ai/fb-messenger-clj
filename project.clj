ed2b8edf0ff3f72682f7b8bd3c6f3b4f1e2de59f
Sebastian Göttschkes <sebastian.goettschkes@googlemail.com>
2017-02-22 14:21:06 +0100

    Add description to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject fb-messenger "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A Clojure Library for the Facebook Messenger Platform"
+  :url "https://github.com/prometheus-ai/fb-messenger-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
