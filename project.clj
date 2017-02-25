bc63f57fc67b03666451293dc9de4b8ad9fb1296
Sebastian Göttschkes <sebastian.goettschkes@googlemail.com>
2017-02-25 20:59:04 +0100

    Adjust project.clj to deploy unsigned to Clojars

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject fb-messenger "0.1.0-SNAPSHOT"
+(defproject fb-messenger "0.1.0"
   :description "A Clojure Library for the Facebook Messenger Platform"
   :url "https://github.com/prometheus-ai/fb-messenger-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :repositories [["clojars" {:url "https://clojars.org/repo/"
+                             :sign-releases false}]])
