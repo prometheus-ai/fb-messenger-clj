7e487ca20a7e24e60361bd0fa3841e76a77fec69
Sebastian Göttschkes <sebastian.goettschkes@googlemail.com>
2017-02-25 21:57:45 +0100

    Add dynamic variable `token` for auth

--- src/fb_messenger/auth.clj
+++ src/fb_messenger/auth.clj
@@ -1,8 +1,17 @@
 (ns fb-messenger.auth)
 
-(defn authenticate [params token]
-  (let [mode (get params "hub.mode")
-        verify-token (get params "hub.verify_token")
-        challenge (get params "hub.challenge")]
-    (when (and (= mode "subscribe") (= verify-token token))
-      challenge)))
+(def ^:dynamic *token* nil)
+
+(defn set-token!
+  [token]
+  (alter-var-root #'*token* (constantly token)))
+
+(defn authenticate
+  ([params]
+   (authenticate params *token*))
+  ([params token]
+   (let [mode (get params "hub.mode")
+          verify-token (get params "hub.verify_token")
+          challenge (get params "hub.challenge")]
+      (when (and (= mode "subscribe") (= verify-token token))
+        challenge))))
--- test/fb_messenger/auth_test.clj
+++ test/fb_messenger/auth_test.clj
@@ -2,7 +2,7 @@
   (:require [clojure.test :refer :all]
             [fb-messenger.auth :refer :all]))
 
-(deftest authenticate-test
+(deftest authenticate-params-token-test
   (testing "Correct case."
     (is (= "test-challenge" (authenticate {"hub.mode" "subscribe", "hub.verify_token" "test-token", "hub.challenge" "test-challenge"} "test-token"))))
 
@@ -11,3 +11,9 @@
 
   (testing "Wrong token"
     (is (= nil (authenticate {"hub.mode" "subscribe", "hub.verify_token" "test-token", "hub.challenge" "test-challenge"} "wrong-token")))))
+
+(deftest authenticate-params-test
+  (testing "Correct case."
+    (do
+      (set-token! "test-token")
+      (is (= "test-challenge" (authenticate {"hub.mode" "subscribe", "hub.verify_token" "test-token", "hub.challenge" "test-challenge"}))))))
