5c30d0873e39af9fb90d2588c26a58844197592a
Sebastian Göttschkes <sebastian.goettschkes@googlemail.com>
2017-03-01 14:36:14 +0100

    Release version 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fb-messenger "0.2.0-SNAPSHOT"
+(defproject fb-messenger "0.2.0"
 
   :description "A Clojure Library for the Facebook Messenger Platform"
   :url "https://github.com/prometheus-ai/fb-messenger-clj"
